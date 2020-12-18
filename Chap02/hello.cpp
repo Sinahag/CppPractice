434ba61f36a71c13718de869cd37c64595dfec37
Sinahag <sina.liana.2001@gmail.com>
2020-12-17 18:02:15 -0800

    Update hello.cpp

--- Chap02/hello.cpp
+++ Chap02/hello.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    int ia[5];
+    ia[0] = 1;
+    ia[1] = 2;
+    printf("this is the first value in the array %d\n", ia[0]);
+    printf("this is the second value in the array %d\n", ia[1]);
+    int* ip = ia;
+    *ip = 5;
+    printf("this is the first value in the array %d\n", ia[0]);
+    printf("this is the second value in the array %d\n", ia[1]);
+    *(++ip) = 9;
+    printf("this is the first value in the array %d\n", ia[0]);
+    printf("this is the second value in the array %d\n", ia[1]);
+    *(++ip) = 23;
+    printf("this is the first value in the array %d\n", ia[0]);
+    printf("this is the second value in the array %d\n", ia[1]);
+    printf("this is the second value in the array %d\n", ia[2]);
     return 0;
 }
